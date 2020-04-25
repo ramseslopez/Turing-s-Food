9724c1ef643302fd3eca2f879f0e4417eef0514a
ramseslopez <ramseslopez99@ciencias.unam.mx>
2020-04-24 21:20:49 -0500

    database change

--- config/settings/local.py
+++ config/settings/local.py
@@ -21,12 +21,8 @@ ALLOWED_HOSTS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tfood',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
